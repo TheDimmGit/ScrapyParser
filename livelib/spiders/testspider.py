3da53a105453ee3389a8a8a4dd7495fd92db63be
TheDimmGit <73245326+TheDimmGit@users.noreply.github.com>
2020-12-02 01:05:42 +0200

    scrapy crawler created

---
+++ livelib/spiders/testspider.py
@@ -0,0 +1,26 @@
+import scrapy
+
+
+class TestspiderSpider(scrapy.Spider):
+    name = 'testspider'
+    allowed_domains = ['https://www.livelib.ru/books/top']
+    start_urls = ['https://www.livelib.ru/books/top']
+
+    def parse(self, response):
+        for item in response.css('.block-border'):
+            title = '.brow-book-name ::text'
+            author = '.brow-book-author ::text'
+            year = '.compact td::text'
+            year_list = [i for i in item.css(year).getall() if str(i).isdigit()]
+            rating = '.rating-value ::text'
+            img = '.cover-wrapper img::attr(data-pagespeed-lazy-src)'
+            link = 'a::attr(href)'
+            yield {
+                'title': item.css(title).get(),
+                'author': item.css(author).get(),
+                'year': year_list[0] if year_list else 'n/a',
+                'rating': item.css(rating).get(),
+                'img': item.css(img).get(),
+                'link': 'https://www.livelib.ru'+str(item.css(link).extract_first()),
+
+            }
